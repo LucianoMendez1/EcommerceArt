42d6481640608991df6afc2c2ad76770deb26a45
luciano mendez <mendezluciano23@gmail.com>
2023-05-27 22:36:54 -0300

    actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,16 @@ import Producto from './components/producto/Producto';
 import Carrito from './components/carrito/Carrito';
 import { stockProductos } from './components/producto/stockProductos';
 import { CartProvider } from './components/carrito/CartContext';
+import Navegacion from './components/navegacion/Navegacion';
 
 const App = () => {
   const [mostrarCarrito, setMostrarCarrito] = useState(true);
 
   return (
     <CartProvider>
-      <div className="nav">
-        <h1>navegacion</h1>
-      </div>
+      <div className="golden-stripe left-golden-stripe"></div>
+      <div className="golden-stripe right-golden-stripe"></div>
+      <Navegacion />
       <div className="app-container">
         <div className="logo">𝓟𝓪𝓵𝓶𝓪𝓐𝓻𝓽</div>
         <div className="productos-container">
