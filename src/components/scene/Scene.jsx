0181f183cc698a33999dab0309ae29ed1362520b
luciano mendez <mendezluciano23@gmail.com>
2023-05-28 22:39:08 -0300

    museo

--- src/components/scene/Scene.jsx
+++ src/components/scene/Scene.jsx
@@ -53,7 +53,7 @@ const Scene = () => {
         // Scene
 
 
-        const ambientLight = new THREE.AmbientLight(0xffffff , 1)
+        const ambientLight = new THREE.AmbientLight(0xffffff ,0.8)
         scene.add(ambientLight)
 
 
