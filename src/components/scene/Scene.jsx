9be93cde760313ae371b2fbee5a9bbc7828f2053
luciano mendez <mendezluciano23@gmail.com>
2023-05-28 22:46:16 -0300

    actualizacion de carrito

--- src/components/scene/Scene.jsx
+++ src/components/scene/Scene.jsx
@@ -53,7 +53,7 @@ const Scene = () => {
         // Scene
 
 
-        const ambientLight = new THREE.AmbientLight(0xffffff ,0.8)
+        const ambientLight = new THREE.AmbientLight(0xffffff ,0.7)
         scene.add(ambientLight)
 
 
