c5e2263f520d6225f4db9cf60d152de6e20a6494
Luciano Mendez <91483691+LucianoMendez1@users.noreply.github.com>
2023-05-30 23:02:31 -0300

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -8,9 +8,9 @@ const Home = () => {
     <div className="home-container">
       <Scene />
       <div className="presentation">
-        <h1 className="title">𝓟𝓪𝓵𝓶𝓪𝓐𝓻t</h1>
-        <p className="description">¡𝓓𝓮𝓼𝓬𝓾𝓫𝓻𝓮 𝓮𝓵 𝓪𝓻𝓽𝓮 𝓮𝓷 𝓬𝓪𝓭𝓪 𝓬𝓻𝓮𝓪𝓬𝓲ó𝓷!</p>
-        <Link to="/productos" className="btn-productos">𝓣𝓲𝓮𝓷𝓭𝓪</Link>
+        <h1 className="title">Palma Art</h1>
+        <p className="description">¡Descubre el arte en cada creacion!</p>
+        <Link to="/productos" className="btn-productos">Tienda</Link>
       </div>
     </div>
   );
