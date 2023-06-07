6d2e851acf9e8e3e64e808dff48a555e841f2e5f
luciano mendez <mendezluciano23@gmail.com>
2023-06-06 22:49:29 -0300

    home actualizacion

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,17 +1,87 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './home.css';
 import Scene from '../scene/Scene';
-import { Link } from 'react-router-dom';
+import { gsap } from 'gsap';
 
 const Home = () => {
+  const [showDescription, setShowDescription] = useState(false);
+
+  const handleMiClick = () => {
+    setShowDescription(true);
+  };
+
+  const titleRef = useRef(null);
+  const descriptionRef = useRef(null);
+  const btnProductosRef = useRef(null);
+
+  useEffect(() => {
+    const timeline = gsap.timeline();
+
+    timeline.fromTo(
+      titleRef.current,
+      { y: 100, opacity: 0 },
+      { y: 0, opacity: 1, duration: 0.9, ease: 'power3.out' }
+    );
+
+    return () => {
+      timeline.kill();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (showDescription) {
+      const timeline = gsap.timeline();
+
+      timeline.fromTo(
+        descriptionRef.current,
+        { y: 50, opacity: 0 },
+        { y: 0, opacity: 1, duration: 0.9, ease: 'power3.out' }
+      );
+
+      timeline.fromTo(
+        btnProductosRef.current,
+        { y: 50, opacity: 0 },
+        { y: 0, opacity: 1, duration: 0.8, ease: 'power3.out' },
+        '-=0.9' 
+      );
+
+      return () => {
+        timeline.kill();
+      };
+    }
+  }, [showDescription]);
+
+  const handleCerrarClick = () => {
+    setShowDescription(false);
+  };
+
   return (
     <div className="home-container">
       <Scene />
       <div className="presentation">
-        <h1 className="title">Palma Art</h1>
-        <p className="description">¡Descubre el arte en cada creacion!</p>
-        <button className="mi">Conoce Mas Sobre Mi</button>
-        <Link to="/productos" className="btn-productos">Tienda</Link>
+        <h1 className="title" ref={titleRef}>
+          Palma Art
+        </h1>
+        {!showDescription && (
+          <button className="mi" onClick={handleMiClick}>
+            Conoce Más Sobre Mí
+          </button>
+        )}
+        {showDescription && (
+          <div className="description-container show" ref={descriptionRef}>
+            <span className="artist-description">
+              Valentina, artista de 21 años con un emprendimiento de venta.
+            </span>
+            <div className="description-buttons">
+              <a href="/productos" className="btn-productos" ref={btnProductosRef}>
+                Tienda
+              </a>
+              <button className="cerrar-button" onClick={handleCerrarClick}>
+                Cerrar
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
